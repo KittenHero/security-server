cc63b045d39d5bf2f95eafcd7ae442ad93342767
Naris <nariscatboy@gmail.com>
2017-08-28 22:40:01 +1000

    added base ui

--- Server.py
+++ Server.py
@@ -1,8 +1,35 @@
-from bottle import get, run
+from bottle import route, redirect, request, response
+from bottle import run
+from bottle import jinja2_template as template
+from bottle import TEMPLATE_PATH
 
-@get('/')
+TEMPLATE_PATH[:] = ['templates']
+
+@route('/')
+@route('/index')
 def index():
-    return 'USSR'
+    user = request.get_cookie('logged_in')
+    if not user:
+        return redirect('login')
+    else:
+        return template('index.html', user=user)
+
+@route('/login', method=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        response.set_cookie(
+                'logged_in',
+                request.forms['user'],
+                max_age=600
+        )
+        return redirect('index')
+    else:
+        return template('login.html')
+
+@route('/logout')
+def logout():
+    response.delete_cookie('logged_in')
+    return redirect('/index')
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
